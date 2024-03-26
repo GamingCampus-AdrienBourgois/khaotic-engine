ccb666faaf1d011def5bec8714182a138db7616d
StratiX0 <jerome.hussenet@sfr.fr>
2024-03-26 12:02:03 +0100

    merge 3d diffuse lighting

--- enginecustom/lightclass.h
+++ enginecustom/lightclass.h
@@ -1,3 +1,4 @@
+#pragma once
 ////////////////////////////////////////////////////////////////////////////////
 // Filename: lightclass.h
 ////////////////////////////////////////////////////////////////////////////////
@@ -18,19 +19,19 @@ using namespace DirectX;
 class LightClass
 {
 public:
-	LightClass();
-	LightClass(const LightClass&);
-	~LightClass();
+    LightClass();
+    LightClass(const LightClass&);
+    ~LightClass();
 
-	void SetDiffuseColor(float, float, float, float);
-	void SetDirection(float, float, float);
-	
-	XMFLOAT4 GetDiffuseColor();
-	XMFLOAT3 GetDirection();
-	
+    void SetDirection(float, float, float);
+    void SetDiffuseColor(float, float, float, float);
+
+    XMFLOAT3 GetDirection();
+    XMFLOAT4 GetDiffuseColor();
+
 private:
-	XMFLOAT4 m_diffuseColor;
-	XMFLOAT3 m_direction;
+    XMFLOAT4 m_diffuseColor;
+    XMFLOAT3 m_direction;
 };
 
 #endif
