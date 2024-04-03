33afb860e28ae7436c9f7461048dcaad78bf9b1e
Mamitiana RASOLOJAONA <iaesensei@yahoo.com>
2024-04-03 10:37:13 +0200

    test render to texture

--- enginecustom/applicationclass.cpp
+++ enginecustom/applicationclass.cpp
@@ -7,6 +7,29 @@ ApplicationClass::ApplicationClass()
 	m_Model = 0;
 	m_LightShader = 0;
 	m_Light = 0;
+<<<<<<< Updated upstream
+=======
+	m_TextureShader = 0;
+	m_Bitmap = 0;
+	m_Sprite = 0;
+	m_Timer = 0;
+	m_MouseStrings = 0;
+	m_FontShader = 0;
+	m_Font = 0;
+	m_TextString1 = 0;
+	m_TextString2 = 0;
+	m_TextString3 = 0;
+	m_Fps = 0;
+	m_FpsString = 0;
+	m_NormalMapShader = 0;
+	m_SpecMapShader = 0;
+	m_RenderCountString = 0;
+	m_ModelList = 0;
+	m_Position = 0;
+	m_Frustum = 0;
+	m_RenderTexture = 0;
+	m_DisplayPlane = 0;
+>>>>>>> Stashed changes
 }
 
 
@@ -43,6 +66,34 @@ bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 	// Create the camera object.
 	m_Camera = new CameraClass;
 	if (!m_Camera)
+	{
+		return false;
+	}
+
+	// Create and initialize the texture shader object.
+	m_TextureShader = new TextureShaderClass;
+
+	result = m_TextureShader->Initialize(m_Direct3D->GetDevice(), hwnd);
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize the texture shader object.", L"Error", MB_OK);
+		return false;
+	}
+
+	// Create and initialize the render to texture object.
+	m_RenderTexture = new RenderTextureClass;
+
+	result = m_RenderTexture->Initialize(m_Direct3D->GetDevice(), 256, 256, SCREEN_DEPTH, SCREEN_NEAR, 1);
+	if (!result)
+	{
+		return false;
+	}
+
+	// Create and initialize the display plane object.
+	m_DisplayPlane = new DisplayPlaneClass;
+
+	result = m_DisplayPlane->Initialize(m_Direct3D->GetDevice(), 1.0f, 1.0f);
+	if (!result)
 	{
 		return false;
 	}
@@ -88,8 +139,29 @@ bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
 void ApplicationClass::Shutdown()
 {
+<<<<<<< Updated upstream
 	// Release the light object.
 	if (m_Light)
+=======
+	// Release the display plane object.
+	if (m_DisplayPlane)
+	{
+		m_DisplayPlane->Shutdown();
+		delete m_DisplayPlane;
+		m_DisplayPlane = 0;
+	}
+
+	// Release the render to texture object.
+	if (m_RenderTexture)
+	{
+		m_RenderTexture->Shutdown();
+		delete m_RenderTexture;
+		m_RenderTexture = 0;
+	}
+
+	// Release the frustum class object.
+	if (m_Frustum)
+>>>>>>> Stashed changes
 	{
 		delete m_Light;
 		m_Light = 0;
@@ -149,6 +221,87 @@ bool ApplicationClass::Frame()
 		return false;
 	}
 
+<<<<<<< Updated upstream
+=======
+	// Get the location of the mouse from the input object,
+	Input->GetMouseLocation(mouseX, mouseY);
+
+	// Check if the mouse has been pressed.
+	mouseDown = Input->IsMousePressed();
+
+	// Update the mouse strings each frame.
+	result = UpdateMouseStrings(mouseX, mouseY, mouseDown);
+	if (!result)
+	{
+		return false;
+	}
+
+	//// Obtenez la position de la souris
+	//Input->GetMouseLocation(mouseX, mouseY);
+
+	//// Calculez la distance parcourue par la souris depuis le dernier frame
+	//float deltaX = mouseX - m_previousMouseX;
+	//float deltaY = mouseY - m_previousMouseY;
+
+	//// Mettez à jour les positions précédentes de la souris
+	//m_previousMouseX = mouseX;
+	//m_previousMouseY = mouseY;
+
+	//// Utilisez deltaX et deltaY pour ajuster la rotation de la caméra
+	//float rotationSpeed = 0.1f;  // Ajustez cette valeur pour changer la vitesse de rotation
+	//float rotationX = m_Camera->GetRotation().x + deltaY * rotationSpeed;
+	//float rotationY = m_Camera->GetRotation().y + deltaX * rotationSpeed;
+
+	//// Mettez à jour la rotation de la caméra
+	//m_Camera->SetRotation(rotationX, rotationY, 0.0f);
+
+	// Update the sprite object using the frame time.
+	m_Sprite->Update(frameTime);
+	// Render the scene to a render texture.
+	result = RenderSceneToTexture(rotation);
+	if (!result)
+	{
+		return false;
+	}
+
+	// Render the final graphics scene.
+	result = Render();
+	if (!result)
+	{
+		return false;
+	}
+	return true;
+}
+
+bool ApplicationClass::RenderSceneToTexture(float rotation)
+{
+	XMMATRIX worldMatrix, viewMatrix, projectionMatrix;
+	bool result;
+
+	m_RenderTexture->SetRenderTarget(m_Direct3D->GetDeviceContext());
+	m_RenderTexture->ClearRenderTarget(m_Direct3D->GetDeviceContext(), 0.0f, 0.5f, 1.0f, 1.0f);
+
+	m_Camera->SetPosition(0.0f, 0.0f, -5.0f);
+	m_Camera->Render();
+
+	m_Direct3D->GetWorldMatrix(worldMatrix);
+	m_Camera->GetViewMatrix(viewMatrix);
+	m_RenderTexture->GetProjectionMatrix(projectionMatrix);
+
+	worldMatrix = XMMatrixRotationY(rotation);
+
+	m_Model->Render(m_Direct3D->GetDeviceContext());
+
+	result = m_TextureShader->Render(m_Direct3D->GetDeviceContext(), m_Model->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, m_Model->GetTexture());
+	if (!result)
+	{
+		return false;
+	}
+
+	m_Direct3D->SetBackBufferRenderTarget();
+	m_Direct3D->ResetViewport();
+
+>>>>>>> Stashed changes
 	return true;
 }
 
@@ -161,7 +314,41 @@ bool ApplicationClass::Render(float rotation)
 
 	// Clear the buffers to begin the scene.
 	m_Direct3D->BeginScene(0.0f, 0.0f, 0.0f, 1.0f);
+	// Setup matrices - Top display plane.
+	worldMatrix = XMMatrixTranslation(0.0f, 1.5f, 0.0f);
 
+	// Render the display plane using the texture shader and the render texture resource.
+	m_DisplayPlane->Render(m_Direct3D->GetDeviceContext());
+
+	result = m_TextureShader->Render(m_Direct3D->GetDeviceContext(), m_DisplayPlane->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, m_RenderTexture->GetShaderResourceView());
+	if (!result)
+	{
+		return false;
+	}
+	
+	// Setup matrices - Bottom left display plane.
+	worldMatrix = XMMatrixTranslation(-1.5f, -1.5f, 0.0f);
+
+	// Render the display plane using the texture shader and the render texture resource.
+	m_DisplayPlane->Render(m_Direct3D->GetDeviceContext());
+
+	result = m_TextureShader->Render(m_Direct3D->GetDeviceContext(), m_DisplayPlane->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, m_RenderTexture->GetShaderResourceView());
+	if (!result)
+	{
+		return false;
+	}
+
+	// Setup matrices - Bottom right display plane.
+	worldMatrix = XMMatrixTranslation(1.5f, -1.5f, 0.0f);
+
+	// Render the display plane using the texture shader and the render texture resource.
+	m_DisplayPlane->Render(m_Direct3D->GetDeviceContext());
+
+	result = m_TextureShader->Render(m_Direct3D->GetDeviceContext(), m_DisplayPlane->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, m_RenderTexture->GetShaderResourceView());
+	if (!result)
+	{
+		return false;
+	}
 	// Generate the view matrix based on the camera's position.
 	m_Camera->Render();
 
--- enginecustom/applicationclass.h
+++ enginecustom/applicationclass.h
@@ -10,6 +10,29 @@
 #include "modelclass.h"
 #include "lightshaderclass.h"
 #include "lightclass.h"
+<<<<<<< Updated upstream
+=======
+#include "lightmapshaderclass.h"
+#include "multitextureshaderclass.h"
+#include "alphamapshaderclass.h"
+#include "bitmapclass.h"
+#include "textureshaderclass.h"
+#include "spriteclass.h"
+#include "timerclass.h"
+#include "fontshaderclass.h"
+#include "fontclass.h"
+#include "textclass.h"
+#include "fpsclass.h"
+#include "inputclass.h"
+#include "normalmapshaderclass.h"
+#include "specmapshaderclass.h"
+#include "modellistclass.h"
+#include "positionclass.h"
+#include "frustumclass.h"
+#include "rendertextureclass.h"
+#include "displayplaneclass.h"
+
+>>>>>>> Stashed changes
 
 /////////////
 // GLOBALS //
@@ -42,7 +65,15 @@ public:
 	void SetSpeed(float speed) { this->speed = speed; };
 
 private:
+<<<<<<< Updated upstream
 	bool Render(float);
+=======
+	bool Render(float, float, float, float);
+	bool RenderSceneToTexture(float);
+	bool UpdateMouseStrings(int, int, bool);
+	bool UpdateFps();
+	bool UpdateRenderCountString(int);
+>>>>>>> Stashed changes
 
 private:
 	D3DClass* m_Direct3D;
@@ -51,7 +82,38 @@ private:
 	IDXGISwapChain* m_swapChain;
 	LightShaderClass* m_LightShader;
 	LightClass* m_Light;
+<<<<<<< Updated upstream
 	float speed = 0.1f;
+=======
+	LightMapShaderClass* m_LightMapShader;
+	MultiTextureShaderClass* m_MultiTextureShader;
+	AlphaMapShaderClass* m_AlphaMapShader;
+	ModelClass* m_Model;
+	TextureShaderClass* m_TextureShader;
+	BitmapClass* m_Bitmap;
+	SpriteClass* m_Sprite;
+    TimerClass* m_Timer;
+	TextClass* m_MouseStrings;
+	LightClass* m_Lights;
+	int m_numLights;
+	FontShaderClass* m_FontShader;
+	TextClass* m_RenderCountString;
+	FontClass* m_Font;
+	TextClass *m_TextString1, *m_TextString2, *m_TextString3;
+	FpsClass* m_Fps;
+	TextClass* m_FpsString;
+	int m_previousFps;
+	NormalMapShaderClass* m_NormalMapShader;
+	SpecMapShaderClass* m_SpecMapShader;
+	ModelListClass* m_ModelList;
+	PositionClass* m_Position;
+	FrustumClass* m_Frustum;
+	XMMATRIX m_baseViewMatrix;
+	float m_previousMouseX;
+	float m_previousMouseY;
+	RenderTextureClass* m_RenderTexture;
+	DisplayPlaneClass* m_DisplayPlane;
+>>>>>>> Stashed changes
 };
 
 #endif
